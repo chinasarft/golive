5482d561bfe48807c4b05c0e5c21f51615ba131f
liumengke <liumengke@qiniu.com>
2018-07-29 13:38:53 +0800

    add amf codec

--- utils/amf/define.go
+++
@@ -1,7 +0,0 @@
-package amf
-
-/*
-Action Message Format
-AMF0 - http://download.macromedia.com/pub/labs/amf/amf0_spec_121207.pdf
-AMF3 - http://download.macromedia.com/pub/labs/amf/amf3_spec_121207.pdf
-*/
